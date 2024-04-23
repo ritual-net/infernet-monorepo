7036c045e859ed9265a3fc7e18a7d870b5485a78
arshan-ritual <arshan@ritual.net>
2024-04-22 20:11:19 -0400

    feat: torch & onnx typed interface with arbitrary shape support

---
+++ projects/infernet_ml/src/infernet_ml/workflows/utils/common_types.py
@@ -0,0 +1,16 @@
+import torch
+
+DTYPES = {
+    "float": torch.float,
+    "double": torch.double,
+    "cfloat": torch.cfloat,
+    "cdouble": torch.cdouble,
+    "half": torch.half,
+    "bfloat16": torch.bfloat16,
+    "uint8": torch.uint8,
+    "int8": torch.int8,
+    "short": torch.short,
+    "int": torch.int,
+    "long": torch.long,
+    "bool": torch.bool,
+}
