4b7fef14713678460c657a5ea9e81ba5f1ffa3b2
arshan-ritual <arshan@ritual.net>
2024-05-25 19:14:10 -0400

    feat: cachedir & versioning & unification of interface to both arwave & hf

--- libraries/infernet_ml/test/test_load_model.py
+++ libraries/infernet_ml/test/test_load_model.py
@@ -1,10 +1,17 @@
+import logging
+import os
+import tempfile
+
 from infernet_ml.utils.model_loader import (
     ArweaveLoadArgs,
     HFLoadArgs,
     LocalLoadArgs,
     ModelSource,
+    load_model,
     parse_load_args,
 )
+
+log = logging.getLogger(__name__)
 
 
 def test_parse_load_args_hf_hub() -> None:
@@ -38,3 +45,60 @@ def test_parse_load_args_local() -> None:
         "model_path": "/path/to/model",
     }
     assert parse_load_args(model_source, cfg) == LocalLoadArgs(path="/path/to/model")
+
+
+def test_download_model_hf_hub_default_cache() -> None:
+    model_source = ModelSource.HUGGINGFACE_HUB
+    load_args = HFLoadArgs(
+        repo_id="Ritual-Net/iris-classification", filename="iris.onnx"
+    )
+    path = load_model(model_source, load_args)
+    assert "huggingface" in path
+    assert "Ritual-Net" in path
+    assert "iris-classification" in path
+    assert "iris.onnx" in path
+
+
+def test_download_model_hf_hub_custom_cache() -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        model_source = ModelSource.HUGGINGFACE_HUB
+        load_args = HFLoadArgs(
+            repo_id="Ritual-Net/iris-classification",
+            filename="iris.onnx",
+            cache_path=temp_dir,
+        )
+        path = load_model(model_source, load_args)
+        log.info(f"Model downloaded to {path}")
+        assert temp_dir in path
+        assert "Ritual-Net" in path
+        assert "iris-classification" in path
+        assert "iris.onnx" in path
+
+
+def test_download_model_arweave_default_cache() -> None:
+    model_source = ModelSource.ARWEAVE
+    repo_id = f"{os.environ['MODEL_OWNER']}/iris-classification"
+    load_args = HFLoadArgs(repo_id=repo_id, filename="iris.onnx")
+    path = load_model(model_source, load_args)
+    assert path.endswith(f"{repo_id}/latest/iris.onnx")
+
+
+def test_download_model_arweave_custom_cache() -> None:
+    with tempfile.TemporaryDirectory() as temp_dir:
+        model_source = ModelSource.ARWEAVE
+        repo_id = f"{os.environ['MODEL_OWNER']}/iris-classification"
+        load_args = HFLoadArgs(
+            repo_id=repo_id, filename="iris.onnx", cache_path=temp_dir
+        )
+        path = load_model(model_source, load_args)
+        log.info(f"Model downloaded to {path}")
+        assert path == f"{temp_dir}/{repo_id}/latest/iris.onnx"
+
+
+def test_download_model_arweave_custom_version() -> None:
+    model_source = ModelSource.ARWEAVE
+    repo_id = f"{os.environ['MODEL_OWNER']}/sample_linreg"
+    filename = "linreg_10_features.onnx"
+    load_args = HFLoadArgs(repo_id=repo_id, filename=filename, version="1.0.0")
+    path = load_model(model_source, load_args)
+    assert path.endswith(f"{repo_id}/1.0.0/{filename}")
