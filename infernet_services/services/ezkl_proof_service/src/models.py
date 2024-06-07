aaa977c528bd83c71d0d8b33a944f1f95d2dfc5a
w-origin-research <w@originsystems.xyz>
2024-06-07 16:06:10 -0400

    precommit fixes

--- infernet_services/services/ezkl_proof_service/src/models.py
+++ infernet_services/services/ezkl_proof_service/src/models.py
@@ -3,10 +3,9 @@ Module containing data models used by the service
 """
 from typing import Optional
 
-from pydantic import BaseModel
-
-from infernet_ml.utils.service_models import HexStr
 from infernet_ml.utils.codec.vector import DataType
+from infernet_ml.utils.service_models import HexStr
+from pydantic import BaseModel
 
 
 class WitnessInputData(BaseModel):
@@ -16,13 +15,13 @@ class WitnessInputData(BaseModel):
     for example, an input tensor of [[1,2],[1,2]] should be flattened to [1,2,1,2],
     and the input_data field would be [[1,2,1,2]]
     """
-    input_data: Optional[list[list[int]| list[float]]] = None
+
+    input_data: Optional[list[list[int] | list[float]]] = None
     input_shape: Optional[list[int]] = None
     input_dtype: DataType = DataType.float
-    output_data: Optional[list[list[int]| list[float]]] = None
+    output_data: Optional[list[list[int] | list[float]]] = None
     output_shape: Optional[list[int]] = None
     output_dtype: DataType = DataType.float
-
 
 
 class ProofRequest(BaseModel):
--- infernet_services/tests/ezkl_proof_service/test_ezkl_proof_service_web2.py
+++ infernet_services/tests/ezkl_proof_service/test_ezkl_proof_service_web2.py
@@ -1,13 +1,14 @@
+import json
 import logging
+import pathlib
+import tempfile
 from typing import Any
+
+import ezkl  # type: ignore
 import pytest
 from dotenv import load_dotenv
 from test_library.infernet_fixture import setup_logging
 from test_library.web2_utils import get_job, request_job
-import json
-import ezkl
-import pathlib
-import tempfile
 
 SERVICE_NAME = "ezkl_proof_service"
 setup_logging()
@@ -23,54 +24,75 @@ async def test_ezkl_proof_service_completion() -> None:
         SERVICE_NAME,
         {
             "witness_data": {
-                "input_data": [[
-                    0.052521463483572006, 0.04962930083274841,
-                    0.0025634586345404387, 0.06335366517305374,
-                    0.04051826521754265, 0.09236890822649002,
-                    0.06505163758993149, 0.04178299382328987,
-                    0.050887223333120346, 0.08090239018201828,
-                    0.08317205309867859, 0.06714846938848495
-                ]],
+                "input_data": [
+                    [
+                        0.052521463483572006,
+                        0.04962930083274841,
+                        0.0025634586345404387,
+                        0.06335366517305374,
+                        0.04051826521754265,
+                        0.09236890822649002,
+                        0.06505163758993149,
+                        0.04178299382328987,
+                        0.050887223333120346,
+                        0.08090239018201828,
+                        0.08317205309867859,
+                        0.06714846938848495,
+                    ]
+                ],
                 "input_shape": [1, 3, 2, 2],
                 "input_dtype": 0,
-                "output_data": [[
-                    0.013130365870893002, 0.02553769201040268,
-                    0.012407325208187103, 0.013771230354905128,
-                    0.042016975581645966, 0.02824574150145054,
-                    0.0006408646586351097, 0.016479281708598137,
-                    0.015838416293263435, 0.010129566304385662,
-                    0.03322179242968559, 0.023092227056622505,
-                    0.02639247477054596, 0.05993044748902321,
-                    0.03353797644376755, 0.016262909397482872,
-                    0.026708658784627914, 0.010445748455822468,
-                    0.012721805833280087, 0.03294740244746208,
-                    0.02022559754550457, 0.03351482003927231,
-                    0.07052753120660782, 0.03701271489262581,
-                    0.020793013274669647, 0.037580132484436035,
-                    0.01678711734712124
-                ]],
+                "output_data": [
+                    [
+                        0.013130365870893002,
+                        0.02553769201040268,
+                        0.012407325208187103,
+                        0.013771230354905128,
+                        0.042016975581645966,
+                        0.02824574150145054,
+                        0.0006408646586351097,
+                        0.016479281708598137,
+                        0.015838416293263435,
+                        0.010129566304385662,
+                        0.03322179242968559,
+                        0.023092227056622505,
+                        0.02639247477054596,
+                        0.05993044748902321,
+                        0.03353797644376755,
+                        0.016262909397482872,
+                        0.026708658784627914,
+                        0.010445748455822468,
+                        0.012721805833280087,
+                        0.03294740244746208,
+                        0.02022559754550457,
+                        0.03351482003927231,
+                        0.07052753120660782,
+                        0.03701271489262581,
+                        0.020793013274669647,
+                        0.037580132484436035,
+                        0.01678711734712124,
+                    ]
+                ],
                 "output_shape": [3, 3, 3],
                 "output_dtype": 0,
             }
-        }
+        },
     )
     jobresult = await get_job(task)
 
     proof: dict[str, Any] = jobresult
 
-    
-    with tempfile.NamedTemporaryFile(mode='w') as temp_file:
+    with tempfile.NamedTemporaryFile(mode="w") as temp_file:
         # Step 4: Write the JSON object to the temporary file
         json.dump(proof, temp_file)
         temp_file.flush()
         verify_success = ezkl.verify(
-            proof_path=temp_file.name, 
-            settings_path= pathlib.Path(__file__).parent / "settings.json", 
-            vk_path= pathlib.Path(__file__).parent / "verifying.key", 
-            srs_path= pathlib.Path(__file__).parent / "kzg.srs"
+            proof_path=temp_file.name,
+            settings_path=pathlib.Path(__file__).parent / "settings.json",
+            vk_path=pathlib.Path(__file__).parent / "verifying.key",
+            srs_path=pathlib.Path(__file__).parent / "kzg.srs",
         )
-        
-        assert verify_success, "was not able to verify"
 
+        assert verify_success, "was not able to verify"
 
     # Open the file for writing.
