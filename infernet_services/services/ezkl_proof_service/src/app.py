812bfb27bad61db48792634af4b6f8f765cabad5
w-origin-research <w@originsystems.xyz>
2024-06-13 19:25:45 -0400

    incorporate suggestions from review

--- infernet_services/services/ezkl_proof_service/src/app.py
+++ infernet_services/services/ezkl_proof_service/src/app.py
@@ -6,14 +6,15 @@ import logging
 import tempfile
 from os import path
 from typing import Any, Optional, cast
-
+from functools import lru_cache
 import ezkl  # type: ignore
-from eth_abi import decode, encode  # type: ignore
+from eth_abi import encode  # type: ignore
 from huggingface_hub import hf_hub_download  # type: ignore
-from infernet_ml.utils.codec.vector import decode_vector, encode_vector
+from infernet_ml.utils.codec.vector import encode_vector
 from infernet_ml.utils.model_loader import ModelSource
 from infernet_ml.utils.service_models import InfernetInput, JobLocation
 from models import ProofRequest, ProvingArtifactsConfig
+from utils import extractProofRequest
 from pydantic import ValidationError
 from quart import Quart, abort
 from quart import request as req
@@ -27,6 +28,7 @@ DUMMY_ADDR = "0x0000000000000000000000000000000000000000"
 SERVICE_PREFIX = "EZKL_PROOF"
 
 
+@lru_cache
 def load_proving_artifacts(config: dict[str, Any]) -> tuple[str, str, str, str, str]:
     """function to load the proving artifacts depending on the config.
 
@@ -227,108 +229,9 @@ def create_app(test_config: Optional[dict[str, Any]] = None) -> Quart:
         data = await req.get_json()
         logger.debug("recieved data: %s", data)
         try:
-            ########################################################
-            #          BEGIN handle Infernet Input Source
-            ########################################################
 
             infernet_input = InfernetInput(**data)
-            match infernet_input.source:
-                case JobLocation.ONCHAIN:
-                    """
-                    We decode onchain input here. Because the shape
-                    of the data of depends on the proving set up, we
-                    need to read the first three flags to determine
-                    what the remainder of the payload is.
-                    """
-
-                    # decode the flags first
-                    has_vk_addr, has_input, has_output = decode(
-                        ["bool", "bool", "bool"],
-                        bytes.fromhex(cast(str, infernet_input.data)),
-                    )
-
-                    """
-                    we keep track of where each field is
-                    relative to the decoded payload. initialize
-                    them to -1
-                    """
-                    vk_addr_offset = -1
-                    input_offset = -1
-                    output_offset = -1
-
-                    data_types = ["bool", "bool", "bool"]
-
-                    if has_vk_addr:
-                        # if vk_addr is specified, it is always first field
-                        vk_addr_offset = 0
-                        # vk_addr is an address
-                        data_types.append("address")
-                    if has_input:
-                        # if input is specified, it is always the field after attestation
-                        input_offset = vk_addr_offset + 1
-                        # input is raw bytes we further decode to a vector
-                        data_types.append("bytes")
-
-                    if has_output:
-                        # if output is specified, it is always the field after input
-                        output_offset = input_offset + 1
-                        # output is raw bytes we further decode to a vector
-                        data_types.append("bytes")
-
-                    # now we know the shape of the data, decode payload
-                    decoded = decode(
-                        data_types, bytes.fromhex(cast(str, infernet_input.data))
-                    )
-
-                    # we dont care about first 3 fields since they are flags
-                    decoded_vals = decoded[3:]
-
-                    # now lets populate a ProofRequest object with our decoded values
-                    proof_request = ProofRequest()
-
-                    if has_vk_addr:
-                        proof_request.vk_address = decoded_vals[vk_addr_offset]
-                        logger.info(f"decoded vk address {proof_request.vk_address}")
-                    if has_input:
-                        # we further decode the input into a vector bere
-                        input_d, input_s, input_val = decode_vector(
-                            decoded_vals[input_offset]
-                        )
-                        logger.info(f"decoded input: {input_d} {input_s} {input_val}")
-                        proof_request.witness_data.input_shape = list(input_s)
-                        proof_request.witness_data.input_data = [
-                            input_val.numpy().flatten().tolist()
-                        ]
-                        proof_request.witness_data.input_dtype = input_d
-
-                    if has_output:
-                        # we further decode the output into a vector here
-                        output_d, output_s, output_val = decode_vector(
-                            decoded_vals[output_offset]
-                        )
-                        logger.info(
-                            f"decoded ouput: {output_d} {output_s} {output_val}"
-                        )
-                        proof_request.witness_data.output_shape = list(output_s)
-                        proof_request.witness_data.output_data = [
-                            output_val.numpy().flatten().tolist()
-                        ]
-                        proof_request.witness_data.output_dtype = output_d
-
-                case JobLocation.ONCHAIN:
-                    proof_request = ProofRequest(
-                        **cast(dict[str, Any], infernet_input.data)
-                    )
-                case _:
-                    abort(
-                        400,
-                        f"Source must either be {JobLocation.ONCHAIN} or {JobLocation.OFFCHAIN}.",  # noqa: E501
-                    )
-
-            ########################################################
-            #          END handle Infernet Input Source
-            ########################################################
-
+            proof_request:ProofRequest = extractProofRequest(infernet_input)
             # parse witness data
             witness_data = proof_request.witness_data
 
---
+++ infernet_services/services/ezkl_proof_service/src/utils.py
@@ -0,0 +1,111 @@
+from infernet_ml.utils.service_models import InfernetInput, JobLocation
+from infernet_ml.utils.codec.vector import decode_vector
+from models import ProofRequest
+from typing import cast, Any
+from eth_abi import decode
+import logging
+
+from quart import abort
+
+logger = logging.getLogger(__file__)
+
+
+def extractProofRequest(infernet_input:InfernetInput)-> ProofRequest:
+    """
+    Helper function to extract a ProofRequest from an EZKL Service 
+    InfernetInput payload.
+    """
+    match infernet_input.source:
+        case JobLocation.ONCHAIN:
+            """
+            We decode onchain input here. Because the shape
+            of the data of depends on the proving set up, we
+            need to read the first three flags to determine
+            what the remainder of the payload is.
+            """
+
+            # decode the flags first
+            has_vk_addr, has_input, has_output = decode(
+                ["bool", "bool", "bool"],
+                bytes.fromhex(cast(str, infernet_input.data)),
+            )
+
+            """
+            we keep track of where each field is
+            relative to the decoded payload. initialize
+            them to -1
+            """
+            vk_addr_offset = -1
+            input_offset = -1
+            output_offset = -1
+
+            data_types = ["bool", "bool", "bool"]
+
+            if has_vk_addr:
+                # if vk_addr is specified, it is always first field
+                vk_addr_offset = 0
+                # vk_addr is an address
+                data_types.append("address")
+            if has_input:
+                # if input is specified, it is always the field after attestation
+                input_offset = vk_addr_offset + 1
+                # input is raw bytes we further decode to a vector
+                data_types.append("bytes")
+
+            if has_output:
+                # if output is specified, it is always the field after input
+                output_offset = input_offset + 1
+                # output is raw bytes we further decode to a vector
+                data_types.append("bytes")
+
+            # now we know the shape of the data, decode payload
+            decoded = decode(
+                data_types, bytes.fromhex(cast(str, infernet_input.data))
+            )
+
+            # we dont care about first 3 fields since they are flags
+            decoded_vals = decoded[3:]
+
+            # now lets populate a ProofRequest object with our decoded values
+            proof_request = ProofRequest()
+
+            if has_vk_addr:
+                proof_request.vk_address = decoded_vals[vk_addr_offset]
+                logger.info(f"decoded vk address {proof_request.vk_address}")
+            if has_input:
+                # we further decode the input into a vector bere
+                input_d, input_s, input_val = decode_vector(
+                    decoded_vals[input_offset]
+                )
+                logger.info(f"decoded input: {input_d} {input_s} {input_val}")
+                proof_request.witness_data.input_shape = list(input_s)
+                proof_request.witness_data.input_data = [
+                    input_val.numpy().flatten().tolist()
+                ]
+                proof_request.witness_data.input_dtype = input_d
+
+            if has_output:
+                # we further decode the output into a vector here
+                output_d, output_s, output_val = decode_vector(
+                    decoded_vals[output_offset]
+                )
+                logger.info(
+                    f"decoded ouput: {output_d} {output_s} {output_val}"
+                )
+                proof_request.witness_data.output_shape = list(output_s)
+                proof_request.witness_data.output_data = [
+                    output_val.numpy().flatten().tolist()
+                ]
+                proof_request.witness_data.output_dtype = output_d
+
+        case JobLocation.ONCHAIN:
+            proof_request = ProofRequest(
+                **cast(dict[str, Any], infernet_input.data)
+            )
+        case _:
+            abort(
+                400,
+                f"Source must either be {JobLocation.ONCHAIN} or {JobLocation.OFFCHAIN}.",  # noqa: E501
+            )
+
+    return proof_request
