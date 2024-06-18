40ccc827f26e03726317c4434b2896ee3ff1c9ea
arshan-ritual <arshan@ritual.net>
2024-06-18 10:54:24 -0400

    feat: docs generation for ezkl docs

--- tools/generate_services_docs.py
+++ tools/generate_services_docs.py
@@ -10,11 +10,24 @@ from generate_docs import add_common_config  # type: ignore
 load_dotenv()
 
 
+def monorepo_dir() -> str:
+    """
+    Get the top level directory of the infernet monorepo.
+
+    Returns:
+        The path to the top level directory
+    """
+    top_level_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    while "infernet-monorepo" not in os.path.basename(top_level_dir):
+        top_level_dir = os.path.dirname(top_level_dir)
+    return top_level_dir
+
+
 def generate_service_docs() -> None:
-    services_root = "infernet_services/services"
-    docs_root = "infernet_services/docs"
-    config_path = "infernet_services/config.yml"
-    output_path = "infernet_services/mkdocs.yml"
+    services_root = f"{monorepo_dir()}/infernet_services/services"
+    docs_root = f"{monorepo_dir()}/infernet_services/docs"
+    config_path = f"{monorepo_dir()}/infernet_services/config.yml"
+    output_path = f"{monorepo_dir()}/infernet_services/mkdocs.yml"
 
     reference_root = os.path.join(docs_root, "reference")
     services = [
@@ -25,13 +38,16 @@ def generate_service_docs() -> None:
     if os.path.exists(reference_root):
         shutil.rmtree(reference_root)
 
+    nav_info = {}
+
     for service in services:
         readme_path = os.path.join(services_root, service, "README.md")
         print(readme_path)
         if os.path.isfile(readme_path):
             reference_service_path = os.path.join(reference_root, f"{service}.md")
-
             os.makedirs(os.path.dirname(reference_service_path), exist_ok=True)
+            capitalized_name = " ".join([a.capitalize() for a in service.split("_")])
+            nav_info[reference_service_path.split("/docs/")[1]] = capitalized_name
             with open(readme_path, "r") as readme_file:
                 content = readme_file.read()
             with open(reference_service_path, "w") as reference_file:
