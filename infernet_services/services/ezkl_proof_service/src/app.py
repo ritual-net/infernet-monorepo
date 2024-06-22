126becfe079019b3ff283874a0b8da59a73f866a
arshan-ritual <arshan@ritual.net>
2024-06-22 14:09:42 -0400

    service fix: precommit

--- infernet_services/services/ezkl_proof_service/src/app.py
+++ infernet_services/services/ezkl_proof_service/src/app.py
@@ -13,13 +13,10 @@ from infernet_ml.utils.codec.ezkl_codec import (
     extract_proof_request,
     extract_visibilities,
 )
+from infernet_ml.utils.ezkl_models import EZKLProofRequest, EZKLProvingArtifactsConfig
 from infernet_ml.utils.ezkl_utils import load_proving_artifacts
 from infernet_ml.utils.model_loader import ModelSource
-from infernet_ml.utils.service_models import (
-    InfernetInput,
-    JobLocation,
-)
-from infernet_ml.utils.ezkl_models import EZKLProofRequest, EZKLProvingArtifactsConfig
+from infernet_ml.utils.service_models import InfernetInput, JobLocation
 from pydantic import ValidationError
 from quart import Quart, abort
 from quart import request as req
