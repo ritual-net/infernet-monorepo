b1764119975f1a20e6b2c2f5b3d69dfc1a02026b
Misha <misha@ritual.net>
2024-06-25 07:48:25 -0400

    marked skipped tests as flaky with retries

--- infernet_services/tests/infernet_node/test_callback.py
+++ infernet_services/tests/infernet_node/test_callback.py
@@ -228,7 +228,7 @@ async def test_infernet_basic_payment_unaccepted_token() -> None:
 
 
 @pytest.mark.asyncio
-@pytest.mark.skip
+@pytest.mark.flaky(retries=3, delay=1)
 async def test_infernet_ignore_subscription_with_low_bid() -> None:
     funding = int(1e18)
     wallet = await create_wallet()
