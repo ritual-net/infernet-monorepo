18fadfd63ca272482069d2e6ad4f0da83aafc142
Stelios Rousoglou <stelios@ritual.net>
2024-09-26 12:56:28 -0400

    fixes

--- infernet_services/tests/torch_inference_service/common.py
+++ infernet_services/tests/torch_inference_service/common.py
@@ -1,7 +1,7 @@
 from typing import Any, Dict
 
 from infernet_ml.services.torch import TorchInferenceRequest
-from infernet_ml.utils.codec.vector import RitualVector
+from infernet_ml.utils.codec.vector import DataType, RitualVector
 import numpy as np
 from test_library.artifact_utils import ar_model_id, hf_model_id
 
@@ -9,7 +9,7 @@ SERVICE_NAME = "torch_inference_service"
 
 
 def california_housing_web2_assertions(r: Dict[str, Any]) -> None:
-    assert r["dtype"] == "double"
+    assert r["dtype"] == DataType.float64
     assert r["shape"] == [1]
     # assert values are close
     assert abs(r["values"][0] - 4.151943055154582) < 1e-6
@@ -18,11 +18,11 @@ def california_housing_web2_assertions(r: Dict[str, Any]) -> None:
 housing_input = RitualVector.from_numpy(
     np.array([8.3252, 41.0, 6.984127, 1.02381, 322.0, 2.555556, 37.88, -122.23])
     .reshape((1, 8))
-    .astype(np.float32)
+    .astype(np.float64)
 )
 
-hf_model = hf_model_id("california-housing", "california-housing.torch")
-ar_model = ar_model_id("california-housing", "california-housing.torch")
+hf_model = hf_model_id("california-housing", "california_housing.torch")
+ar_model = ar_model_id("california-housing", "california_housing.torch")
 
 hf_request = TorchInferenceRequest(
     ml_model=hf_model,
--- infernet_services/tests/torch_inference_service/conftest.py
+++ infernet_services/tests/torch_inference_service/conftest.py
@@ -1,11 +1,15 @@
+import logging
 import os
 from typing import Generator
 
 import pytest
 from dotenv import load_dotenv
-from test_library.config_creator import ServiceConfig
+from test_library.artifact_utils import ar_model_id, hf_model_id
+from test_library.config_creator import ServiceConfig, create_default_config_file
 from test_library.constants import skip_contract, skip_deploying, skip_teardown
 from test_library.infernet_fixture import handle_lifecycle
+
+from infernet_ml.services.torch import TorchServiceConfig
 
 load_dotenv()
 
@@ -18,37 +22,54 @@ TORCH_SERVICE_DOCKER_IMG = (
 )
 TORCH_WITH_PROOFS = "torch_inference_service_with_proofs"
 
+log = logging.getLogger(__name__)
+
+services = [
+    ServiceConfig.build(
+        TORCH_ARWEAVE_PRELOADED,
+        image_id=TORCH_SERVICE_DOCKER_IMG,
+        port=3000,
+        env_vars=TorchServiceConfig(
+            DEFAULT_MODEL_ID=ar_model_id(
+                "california-housing", "california_housing.torch"
+            )
+        ).to_env_dict(),
+    ),
+    ServiceConfig.build(
+        TORCH_HF_PRELOADED,
+        image_id=TORCH_SERVICE_DOCKER_IMG,
+        port=3001,
+        env_vars=TorchServiceConfig(
+            DEFAULT_MODEL_ID=hf_model_id(
+                "california-housing", "california_housing.torch"
+            )
+        ).to_env_dict(),
+    ),
+    ServiceConfig.build(
+        TORCH_SERVICE_NOT_PRELOADED,
+        image_id=TORCH_SERVICE_DOCKER_IMG,
+        port=3000,
+    ),
+    ServiceConfig.build(
+        TORCH_WITH_PROOFS,
+        image_id=TORCH_SERVICE_DOCKER_IMG,
+        port=3003,
+        generates_proofs=True,
+    ),
+]
+
 
 @pytest.fixture(scope="session", autouse=True)
 def torch_setup() -> Generator[None, None, None]:
     yield from handle_lifecycle(
-        [
-            ServiceConfig.build(
-                TORCH_ARWEAVE_PRELOADED,
-                image_id=TORCH_SERVICE_DOCKER_IMG,
-                port=3000,
-                env_vars={},
-            ),
-            ServiceConfig.build(
-                TORCH_HF_PRELOADED,
-                image_id=TORCH_SERVICE_DOCKER_IMG,
-                port=3001,
-                env_vars={},
-            ),
-            ServiceConfig.build(
-                TORCH_SERVICE_NOT_PRELOADED,
-                image_id=TORCH_SERVICE_DOCKER_IMG,
-                port=3002,
-            ),
-            ServiceConfig.build(
-                TORCH_WITH_PROOFS,
-                image_id=TORCH_SERVICE_DOCKER_IMG,
-                port=3003,
-                generates_proofs=True,
-            ),
-        ],
+        services,
         service_wait_timeout=int(os.environ.get("SERVICE_WAIT_TIMEOUT", 60)),
         skip_deploying=skip_deploying,
         skip_contract=skip_contract,
         skip_teardown=skip_teardown,
     )
+
+
+if __name__ == "__main__":
+    log.info("Creating config file")
+    create_default_config_file(services)
