2c16b85b3f92c82b57503d3017b7718a7c984b33
arshan-ritual <arshan@ritual.net>
2024-10-25 07:02:03 -0400

    fix: mark test as flaky.

--- infernet_services/tests/hf_inference_client_service/test_hf_inference_client_service.py
+++ infernet_services/tests/hf_inference_client_service/test_hf_inference_client_service.py
@@ -103,7 +103,7 @@ long_text = """
 
 
 @pytest.mark.asyncio
-@pytest.mark.flaky(retries=3, delay=1)
+@pytest.mark.flaky(retries=5, delay=2)
 async def test_hf_inference_client_service_summarization() -> None:
     min_length_tokens = 28
     max_length_tokens = 56
